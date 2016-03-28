5c8d1c1a32893860604b70a3c7e632afeb8fbd9d
佟野 <1215353560@qq.com>
2016-03-28 20:22:48 +0800

    引导页

--- app/src/main/java/com/lanou/testone/MainActivity.java
+++ app/src/main/java/com/lanou/testone/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //正在做欢迎页
+    //欢迎页完成
+    //我要看看引导页
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
