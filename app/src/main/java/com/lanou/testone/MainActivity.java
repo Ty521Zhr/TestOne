392fb46b47d833a42615d0d8be74a274f9f1bf0a
佟野 <1215353560@qq.com>
2016-03-28 20:20:52 +0800

    欢迎页正在完成

--- app/src/main/java/com/lanou/testone/MainActivity.java
+++ app/src/main/java/com/lanou/testone/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //正在做欢迎页
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
